83a4b4495838c69bb98d5129e2b1de94c013c935
Helge Haveresch <hhaveresch@iup.physik.uni-bremen.de>
2026-05-22 16:11:20 +0200

    separate read in routines for 365nm and 310nm files

--- lp_doas.py
+++ lp_doas.py
@@ -1,7 +1,45 @@
 import pandas as pd
 import os
 
-def read_lpdoas(dir, date, filename=None, mode= "ppb"):
+def read_lpdoas_310(dir, date, filename=None, mode= "ppb"):
+    """
+    Reads LP-DOAS .dat file for a given date, skipping bad lines.
+
+    Parameters
+    ----------
+    file_path : str
+        The path to the LP-DOAS .dat file.
+    date : str
+        The date for which to read the data (format: 'YYMMDD').
+    mode : str
+        The mode for reading the data ('ppb' or 'SC') (default: 'ppb').
+
+    Returns
+    -------
+    df : pd.DataFrame
+        DataFrame with parsed LP-DOAS data.
+    """
+    if filename is None:
+        filename = r"20{}_310_Eval_{}.dat".format(date, mode)
+    file_path = os.path.join(dir, filename)
+    df = pd.read_csv(
+        file_path,
+        sep='\t',
+        engine='python',
+        on_bad_lines='skip'  # skips lines with wrong number of columns
+    )
+    # Convert 'StartDateAndTime' to datetime and set timezone to UTC
+    df['StartDateAndTime'] = pd.to_datetime(df['StartDateAndTime'])
+    if df['StartDateAndTime'].dt.tz is None:
+        df['StartDateAndTime'] = df['StartDateAndTime'].dt.tz_localize('UTC')
+    else:
+        df['StartDateAndTime'] = df['StartDateAndTime'].dt.tz_convert('UTC')
+    df.set_index("StartDateAndTime", inplace=True)
+    df.sort_index(inplace=True)
+
+    return df
+
+def read_lpdoas_365(dir, date, filename=None, mode= "ppb"):
     """
     Reads LP-DOAS .dat file for a given date, skipping bad lines.
 
@@ -36,6 +74,7 @@ def read_lpdoas(dir, date, filename=None, mode= "ppb"):
         df['StartDateAndTime'] = df['StartDateAndTime'].dt.tz_convert('UTC')
     df.set_index("StartDateAndTime", inplace=True)
     df.sort_index(inplace=True)
+
     return df
 
 def mask_lp_doas_file(df_lp_doas, start_time, end_time, rms_threshold = 0.0005):
