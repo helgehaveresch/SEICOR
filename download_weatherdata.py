e9f5d6456930e4bc57f93179b7626760f141d096
Helge Haveresch <hhaveresch@iup.physik.uni-bremen.de>
2026-05-22 16:08:18 +0200

    download meteostat weather station data script

--- download_weatherdata.py
+++ download_weatherdata.py
@@ -16,3 +16,19 @@ data = data.fetch()
 Path(r"Q:\BREDOM\SEICOR\weatherstations\York-Moorende").mkdir(parents=True, exist_ok=True)
 data.to_csv(r"Q:\BREDOM\SEICOR\weatherstations\York-Moorende\weatherdata_hourly.csv")
 # %%
+from datetime import datetime
+import meteostat as ms
+
+# Define time period
+start = datetime(2024, 1, 1)
+end   = datetime(2024, 1, 7)
+
+# Create a daily time series for station D2465
+ts = ms.Daily("D2465", start, end)
+
+
+# %%
+import meteostat as ms
+
+
+# %%
